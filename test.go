6b5a2cc05660c3a601c87757720c2c441ba21a96
Nodir <nodirxakimov@yandex.ru>
2021-09-28 22:59:35 +0500

    Learned returning multiple values from function

--- test.go
+++ test.go
@@ -2,26 +2,44 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strings"
 )
 
-func main() {
-	var slice = []string{"Nodir", "Shokir", "Dilshod", "Husan", "Izzat", "Shohruh"}
-	// slice = append(slice, "Sirojiddin")
-	// fmt.Println(slice, len(slice))
-	// rangeOne := slice[1:3]
-	// rangeTwo := slice[3:]
-	// fmt.Println(rangeOne, len(rangeOne), len(slice))
-	// fmt.Println(rangeTwo, len(rangeTwo), len(slice))
-
-	// greeting := "Hello there! How are you doing?"
-	// fmt.Println(strings.Contains(greeting, "ello"))
-	// fmt.Println(strings.Index(greeting, "o"))
-	// fmt.Println(len(strings.Split(greeting, "")))
-
-	// sort.Strings(slice)
-	// fmt.Println(sort.SearchStrings(slice, "Nodir"))
-
-	for index, value := range slice {
-		fmt.Printf("The value of the %v element is %v \n", index, value)
+func sayGreeting(n string) {
+	fmt.Printf("Good morning %v \n", n)
+}
+func sayBye(n string) {
+	fmt.Printf("Goodbye %v \n", n)
+}
+func cycleNames(n []string, f func(string)) {
+	for _, v := range n {
+		f(v)
 	}
+}
+func circleArea(r float64) float64 {
+	return math.Pi * r * r
+}
+func getInitials(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
+	var initials []string
+	for _, v := range names {
+		initials = append(initials, v[:1])
+	}
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+	return initials[0], "_"
+}
+func main() {
+	names := []string{"Nodir", "Dilshod", "Sherzod", "Hasanboy", "Bahodir", "Xasanboy"}
+	cycleNames(names, sayGreeting)
+	cycleNames(names, sayBye)
+	area := circleArea(12.12)
+	fmt.Printf("Area of the circle is %0.2f\n", area)
+	fn1, sn1 := getInitials("Nodir Xakimov")
+	fn2, sn2 := getInitials("Dilshod")
+	fmt.Println(fn1, sn1)
+	fmt.Println(fn2, sn2)
 }
