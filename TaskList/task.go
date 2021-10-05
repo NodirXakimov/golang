cea914d06b47f953e86b995b6dc49bc7b2b49085
Nodir <nodirxakimov@yandex.ru>
2021-10-05 17:41:13 +0500

    getTask, getTasks, createTask methods are created

--- TaskList/task.go
+++ TaskList/task.go
@@ -1,28 +1,72 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 type Task struct {
-	ID        int
-	Name      string
-	Status    string
-	Priority  string
-	CreatedAt string
-	CreatedBy string
-	DueDate   string
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Status    string `json:"status"`
+	Priority  string `json:"priority"`
+	CreatedAt string `json:"created_at"`
+	CreatedBy string `json:"created_by"`
+	DueDate   string `json:"due_date"`
 }
 
 var tasks []Task
 
+// Get all tasks
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+// Get single task
+func getTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get params
+	// Loop through contacts and find by id
+	for _, task := range tasks {
+		if task.ID == params["id"] {
+			json.NewEncoder(w).Encode(task)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Task{})
+}
+
+// Create new task
+func createTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var task Task
+	_ = json.NewDecoder(r.Body).Decode(&task)
+	task.ID = strconv.Itoa(len(tasks) + 1) // Mock ID - not safe
+	t := time.Now()
+	task.CreatedAt = t.String()
+	tasks = append(tasks, task)
+	json.NewEncoder(w).Encode(task)
+}
+
 func main() {
 	fmt.Println("Hello World!")
-	fmt.Println("Do you check your progress")
+	tasks = append(tasks, Task{"1", "First task", "init", "high", "1", "1", "1"})
+	tasks = append(tasks, Task{"2", "Second task", "test", "middle", "2", "2", "2"})
 
 	// Init Router
-	// r := mux.NewRouter()
+	r := mux.NewRouter()
 
 	// Route Handlers / Endpoints
-	// r.HandleFunc("api/")
+	r.HandleFunc("/api/tasks", getTasks).Methods("GET")
+	r.HandleFunc("/api/tasks/{id}", getTask).Methods("GET")
+	r.HandleFunc("/api/tasks", createTask).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8009", r))
 }
